918b94e6801e408765a28d94912f3e15c51aa08d
ZARIFMAC <uzbzarif@gmail.com>
2023-01-04 10:19:11 +0500

    getupdate

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import os
 import requests
 TOKEN = os.environ['TOKEN']
 
-url =  f'https://api.telegram.org/bot{TOKEN}/getUpdates'
-r = requests.get(url)
-print(r.json())
+def getUpdates():
+    url =  f'https://api.telegram.org/bot{TOKEN}/getUpdates'
+    r = requests.get(url)
+    data = r.json()
+    updates = data['result']
+    return updates
+
+
+upd = getUpdates()
+print(upd)
+
