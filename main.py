d9591b748155a18c4370a6ddfb2ec00e86fa4940
ZARIFMAC <uzbzarif@gmail.com>
2023-01-04 11:23:38 +0500

    main function

--- main.py
+++ main.py
@@ -17,21 +17,26 @@ def sendMessage(chat_id:str, text:str):
     data = r.json()
     return data
 
-upd_len = len(getUpdates())
-upd_new = 0
-while True:
-    update = getUpdates()[-1]
-    # Get user id
-  
-    upd_len = len(getUpdates())
-    if upd_len != upd_new:
-        upd_new = upd_len
-        text = update['message']['text'] # Get text
-        chat_id = update['message']['chat']['id'] # Get chat id
-        # Send message
-        sendMessage(chat_id, text)
-        print(text)
 
-    print(f'upd_len = {upd_len}, upd_new = {upd_new}')
-    sleep(2)
+def main():
+    upd_len = len(getUpdates())
+    upd_new = 0
+    while True:
+        update = getUpdates()[-1]
+        # Get user id
     
+        upd_len = len(getUpdates())
+        if upd_len != upd_new:
+            upd_new = upd_len
+            text = update['message']['text'] # Get text
+            chat_id = update['message']['chat']['id'] # Get chat id
+        
+            # Send message
+            sendMessage(chat_id, text)
+            print(text)
+
+        print(f'upd_len = {upd_len}, upd_new = {upd_new}')
+        sleep(2)
+    
+if __name__ == '__main__':
+    main()
