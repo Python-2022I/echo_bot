ad834bc4e63cad1d1e6a570a68c89327912ca5dd
ZARIFMAC <uzbzarif@gmail.com>
2023-01-04 11:17:43 +0500

    echo bot

--- main.py
+++ main.py
@@ -11,15 +11,25 @@ def getUpdates():
     updates = data['result']
     return updates
 
+def sendMessage(chat_id:str, text:str):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    r = requests.get(url,params={'chat_id':chat_id, 'text':text})
+    data = r.json()
+    return data
 
 upd_len = len(getUpdates())
 upd_new = 0
 while True:
     update = getUpdates()[-1]
+    # Get user id
+  
     upd_len = len(getUpdates())
     if upd_len != upd_new:
         upd_new = upd_len
-        text = update['message']['text']
+        text = update['message']['text'] # Get text
+        chat_id = update['message']['chat']['id'] # Get chat id
+        # Send message
+        sendMessage(chat_id, text)
         print(text)
 
     print(f'upd_len = {upd_len}, upd_new = {upd_new}')
