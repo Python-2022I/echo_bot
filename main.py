85d5efc3e4cd86cbe4158e3ff58c4d30cce5f34d
ZARIFMAC <uzbzarif@gmail.com>
2023-01-04 11:15:42 +0500

    get text

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import os
 import requests
+from pprint import pprint
+from time import sleep
 TOKEN = os.environ['TOKEN']
 
 def getUpdates():
     url =  f'https://api.telegram.org/bot{TOKEN}/getUpdates'
-    r = requests.get(url)
+    r = requests.get(url,params={'limit':5})
     data = r.json()
     updates = data['result']
     return updates
 
 
-upd = getUpdates()
-print(upd)
+upd_len = len(getUpdates())
+upd_new = 0
+while True:
+    update = getUpdates()[-1]
+    upd_len = len(getUpdates())
+    if upd_len != upd_new:
+        upd_new = upd_len
+        text = update['message']['text']
+        print(text)
 
+    print(f'upd_len = {upd_len}, upd_new = {upd_new}')
+    sleep(2)
+    
